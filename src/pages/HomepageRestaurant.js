904f6c3e0221737cdc52a5e2e832e0c5255f9e41
helia-vafaei <h.vafaie123@gmail.com>
2023-10-20 11:58:09 +0330

    add : adding animation in HomepageRestaurant

--- src/pages/HomepageRestaurant.js
+++ src/pages/HomepageRestaurant.js
@@ -558,7 +558,7 @@ function HomepageRestaurant(props){
                                                 </div>
                                             </Grid>
                                             <Grid item md={6}>
-                                                <CardContent>
+                                                <CardContent className='home-restaurant-animation'>
                                                     <Grid container style={{color: "black"}}>
                                                         <Grid item>
                                                             <Typography className='restaurant-name-hemepage-restaurant' gutterBottom>
