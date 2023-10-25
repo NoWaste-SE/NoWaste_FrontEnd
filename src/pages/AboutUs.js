2b908f4c1c855d4d40ccb3c055624f5dcbfeccc6
haniyehasadi <haniyeasadi627@gmail.com>
2023-10-25 19:25:34 +0330

    fix: clean code applied for about us page- change structure of page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,14 +7,12 @@ import StepContent from '@mui/material/StepContent';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import { Grid } from '@mui/material';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Stack from '@mui/material/Stack';
 import "./AboutUs.css";
 import HeaderCustomer from "../components/HeaderCustomer";
-
+import { Grid } from "@material-ui/core";
+import { useEffect } from "react";
 
 const steps = [
     {
@@ -54,42 +52,30 @@ const AboutUs = () => {
     
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
-
     };
+
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
+
     const handleReset = () => {
         setActiveStep(0);
-
     };
+
     return ( 
         <div >
             <HeaderCustomer />
-            <h2 className='about-us-title'>About Nowaste</h2>
-            {/* <Grid container spacing={2}>
-                <Grid item lg={6} md={6} sm={6} className='about-us-about'>
-                    NoWaste is a food distribution app that connects customers with restaurants to offer high-quality restaurant food at affordable prices while reducing food waste.
-                    <br></br>
-                    By leveraging the app, customers have access to surplus food from restaurants that would otherwise go to waste, allowing them to enjoy delicious meals at a lower cost.
-                    The primary goal of NoWaste is to foster a sustainable food ecosystem by providing a platform for restaurants to communicate their excess food availability and for customers to place orders.
-                    Through this app, restaurants can effectively manage their inventory, ensuring that no food goes to waste and optimizing their operations.
-                    The dedicated team of developers behind NoWaste is committed to curbing food waste and promoting sustainability in the food industry.
-                    <br></br>
-                    Their continuous efforts involve implementing innovative solutions and collaborating with restaurants to streamline the process of food preparation and distribution.
-                    By facilitating efficient communication and coordination, NoWaste aims to minimize food wastage on a significant scale, benefiting both the environment and the economy.
-                    With the user-friendly interface and seamless user experience, NoWaste makes it convenient for customers to order surplus food while contributing to the larger mission of reducing food waste globally.
-                    Join the NoWaste community today and become part of the movement towards a more sustainable and responsible approach to food consumption.
-                </Grid>    
-                <Grid item lg={6} md={6} sm={6}>
-                    <img src="/about-us-1.jpg" alt="ImageOfAboutUs" className='about-us-image'/>
-                </Grid>
-            </Grid> */}
-            <Stack direction="row" spacing={2}>
-                <div className='about-us-stack'>
-                    <p>
+            <Typography 
+                variant='h4' 
+                className='about-us-title'
+            >
+                About Nowaste
+            </Typography>
+            <Grid container spacing={2} className='about-us-description'>
+                <Grid item lg={10} md={9}>
+                    <Typography>
                         NoWaste is a food distribution app that connects customers with restaurants to offer high-quality restaurant food at affordable prices while reducing food waste.
-                        <br></br>
+                        <br />
                         By leveraging the app, customers have access to surplus food from restaurants that would otherwise go to waste, allowing them to enjoy delicious meals at a lower cost.
                         The primary goal of NoWaste is to foster a sustainable food ecosystem by providing a platform for restaurants to communicate their excess food availability and for customers to place orders.
                         Through this app, restaurants can effectively manage their inventory, ensuring that no food goes to waste and optimizing their operations.
@@ -99,105 +85,197 @@ const AboutUs = () => {
                         By facilitating efficient communication and coordination, NoWaste aims to minimize food wastage on a significant scale, benefiting both the environment and the economy.
                         With the user-friendly interface and seamless user experience, NoWaste makes it convenient for customers to order surplus food while contributing to the larger mission of reducing food waste globally.
                         Join the NoWaste community today and become part of the movement towards a more sustainable and responsible approach to food consumption.
-                    </p>
-                    <img src="/about-us-1.jpg" alt="ImageOfAboutUs" className='about-us-image-title'/>
-                </div>
-            </Stack>
-            <h2 className='about-us-title'>NoWaste process</h2>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Box sx={{ maxWidth: 400 }} >
-                    <Stepper activeStep={activeStep} orientation="vertical" >
-                        {steps.map((step, index) => (
+                    </Typography>
+                </Grid>
+                <Grid item lg={2} md={3}>
+                    <img 
+                        src="/about-us-1.jpg" 
+                        alt="ImageOfAboutUs" 
+                        className='image'
+                    />
+                </Grid>
+            </Grid>
+            <Typography 
+                variant='h4' 
+                className='about-us-title'
+            >
+                NoWaste Process
+            </Typography>
+            <Box
+                className='about-us-steps'
+            >
+                <Stepper 
+                    activeStep={activeStep} 
+                    orientation="vertical" 
+                >
+                    {steps.map((step, index) => (
                         <Step key={step.label}>
                             <StepLabel
-                            StepIconProps={{
-                                classes: {
-                                    root: 'custom-step-icon',
-                                    active: 'custom-step-icon-active',
-                                },
-                            }}    
-                            optional={
-                                index === 6 ? (
-                                <Typography variant="caption">Last step</Typography>
-                                ) : null
-                            }
+                                StepIconProps={{
+                                    classes: {
+                                        root: 'custom-step-icon',
+                                        active: 'custom-step-icon',
+                                    }
+                                }}    
+                                optional={
+                                    index === steps.length -1 ? (
+                                        <Typography variant="caption">
+                                            Last step
+                                        </Typography>
+                                    ) : null
+                                }
                             >
-                            {step.label}
+                                {step.label}
                             </StepLabel>
                             <StepContent>
-                            <Typography>{step.description}</Typography>
-                            <Box sx={{ mb: 2 }}>
-                                <div>
-                                <Button
-                                    variant="contained"
-                                    onClick={handleNext}
-                                    sx={{ mt: 1, mr: 1 }}
-                                    className="about-us-continue-button"
-                                >
-                                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                                </Button>
-                                <Button
-                                    variant="contained"
-                                    className="about-us-continue-button"
-                                    disabled={index === 0}
-                                    onClick={handleBack}
-                                    sx={{ mt: 1, mr: 1 }}
-                                >
-                                    Back
-                                </Button>
-                                </div>
-                            </Box>
+                                <Typography>
+                                    {step.description}
+                                </Typography>
+                                <Box sx={{ mb: 2 }}>
+                                    <Button
+                                        variant="contained"
+                                        onClick={handleNext}
+                                        sx={{ mt: 1, mr: 1 }}
+                                        className="about-us-button"
+                                    >
+                                        {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        className="about-us-button"
+                                        disabled={index === 0}
+                                        onClick={handleBack}
+                                        sx={{ mt: 1, mr: 1 }}
+                                    >
+                                        Back
+                                    </Button>
+                                </Box>
                             </StepContent>
                         </Step>
-                        ))}
-                    </Stepper>
-                    {activeStep === steps.length && (
-                        <Paper square elevation={0} sx={{ p: 3 }}>
-                        {/* <Typography>All steps completed - you&apos;re finished</Typography> */}
-                        <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }} variant="contained" className="about-us-continue-button">
+                    ))}
+                </Stepper>
+                {activeStep === steps.length && (
+                    <Paper square elevation={0} sx={{ p: 3 }}>
+                        <Button 
+                            onClick={handleReset} 
+                            sx={{ mt: 1, mr: 1 }} 
+                            variant="contained" 
+                            className="about-us-button"
+                        >
                             Reset
                         </Button>
-                        </Paper>
-                    )}
-                    </Box>
-                </div>
-                <h2 className='about-us-title'>About team</h2>
-                <Stack direction="row" spacing={3} >
-                    <div className='about-us-team-top'>
-                        <div className='about-us-image-container'>
-                        <img src='/haniye.jpg' className='about-us-haniye' alt='Hanieh asady'></img>
-                        <a href='https://github.com/HaniyeAsadi' className='about-us-link'>Haniye As'adi (Front-end)</a>
-                        </div>
-                        <div className='about-us-image-container'>
-                        <img src='/helia.png' className='about-us-helia' alt='Helia Vafaei'></img>
-                        <a href='https://github.com/helia-vafaei' className='about-us-link'>Helia Vafaei (Front-end)</a>
-                        </div>
-                        <div className='about-us-image-container'>
-                        <img src='/setareh.jpg' className='about-us-setareh' alt='Setareh Babajani'></img>
-                        <a href='https://github.com/setarehbabajani' className='about-us-link'>Setareh Babajani (Front-end)</a>
-                        </div>
-                    </div>
-                </Stack>
-
-                <Stack direction="row" spacing={3} >
-                    <div className='about-us-team-bottom'>
-                        <div className='about-us-image-container'>
-                        <img src='/niayesh.jpg' className='about-us-niayesh' alt='Niayesh Khani'></img>
-                        <a href='https://github.com/niayesh-khani' className='about-us-link'>Niayesh Khani (Front-end)</a>
-                        </div>
-                        <div className='about-us-image-container'>
-                        <img src='/negin.jpg' className='about-us-negin' alt='Negin Haghighi'></img>
-                        <a href='https://github.com/haghighy' className='about-us-link-negin'>Negin Haghighi (Back-end)</a>
-                        </div>
-                        <div className='about-us-image-container'>
-                        <img src='/golbarg.jpg' className='about-us-golbarg' alt='Golbarg Sepehara'></img>
-                        <a href='https://github.com/golbara/' className='about-us-link-golbarg'>Golbarg Sepehara (Back-end)</a>
-                        </div>
-                    </div>
-                </Stack>
+                    </Paper>
+                )}
+            </Box>
+            <Typography 
+                variant='h4' 
+                className='about-us-title'
+            >
+                About Team
+            </Typography>
+            <Grid container spacing={3} style={{width: '100%', paddingBottom: '-50px'}}>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/haniye.jpg' 
+                        alt='Haniye asadi'
+                    />
+                    <a 
+                        href='https://github.com/HaniyeAsadi' 
+                        className='link'
+                    >
+                        Haniye As'adi (Front-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6}
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/helia.png' 
+                        alt='Helia Vafaei'
+                    />
+                    <a 
+                        href='https://github.com/helia-vafaei' 
+                        className='link'
+                    >
+                        Helia Vafaei (Front-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/setareh.jpg' 
+                        alt='Setareh Babajani'
+                    />
+                    <a 
+                        href='https://github.com/setarehbabajani' 
+                        className='link'
+                    >
+                        Setareh Babajani (Front-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/negin.jpg' 
+                        alt='Negin Haghighi'
+                    />
+                    <a 
+                        href='https://github.com/haghighy' 
+                        className='link'
+                    >
+                        Negin Haghighi (Back-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/golbarg.jpg' 
+                        alt='Golbarg Sepehara'
+                    />
+                    <a 
+                        href='https://github.com/golbara/' 
+                        className='link'
+                    >
+                        Golbarg Sepehara (Back-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/Melika.jpg' 
+                        alt='Melika Mohamadi Fakhar'
+                    />
+                    <a 
+                        href='https://github.com/melikamohamadifakhar' 
+                        className='link'
+                    >
+                        Melika Mohamadi Fakhar (Back-end)
+                    </a>
+                </Grid>
+                <Grid item lg={4} md={4} sm={6} 
+                    className='about-us-member'
+                >
+                    <img 
+                        src='/AboutUs/niayesh.jpg' 
+                        alt='Setareh Babajani'
+                        className='niyayesh'
+                    />
+                    <a 
+                        href='https://github.com/niayesh-khani' 
+                        className='link'
+                    >
+                        Niayesh Khani (Front-end)
+                    </a>
+                </Grid>
+            </Grid>
             <Footer/>
-        </div>
-        
+        </div> 
     );
 }
 
