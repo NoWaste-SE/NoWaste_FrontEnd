3c1a8f085a561f585771181021f1b68b380e1718
setarehbabajani <setarehbabajani81@gmail.com>
2023-11-16 11:13:20 +0330

    add: adding requests

--- src/components/Shopping card/ShoppingCard.js
+++ src/components/Shopping card/ShoppingCard.js
@@ -1,52 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Grid, Typography } from "@material-ui/core";
-import "./ShoppingCard.css"
+import axios from "axios";
+import "./ShoppingCard.css";
 
 function ShoppingCard() {
-  // Mock data for two restaurants
-    const restaurants = [
-        {
-        id: 1,
-        name: "KFC",
-        image: "kfc.jpg",
-        orders: [
-            { itemName: "Chicken Burger", count: 2, price: 5.99 },
-            { itemName: "Fries", count: 1, price: 2.49 },
-        ],
-        },
-        {
-        id: 2,
-        name: "Pizza Hut",
-        image: "pizzahut.jpg",
-        orders: [
-            { itemName: "Pepperoni Pizza", count: 1, price: 12.99 },
-            { itemName: "Garlic Bread", count: 1, price: 4.99 },
-        ],
-        },
-    ];
+    const token = localStorage.getItem("token");
+    const [allOrders, setAllOrders] = useState([]);
+    useEffect(() => {
+        axios.get(
+            `http://188.121.124.63/user/orders/`, 
+            {headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PATCH',
+                'Authorization': 'Bearer ' + token.slice(1, -1),
+            }}
+        )
+        .then((response) => {
+            setAllOrders(response.data);
+        })
+        .catch((error) => {
+            console.log(error)});
+    }, []);
 
     return (
         <>
-        {restaurants.map((restaurant) => (
-            <Box key={restaurant.id} className="shopping-card-restaurant-box">
+        {allOrders.map((restaurant) => (
+            <Box key={restaurant.restaurant.id} className="shopping-card-restaurant-box">
             <Grid container spacing={2}>
                 <Grid item lg={4} md={4} sm={4} xs={12}>
                 <img
                     className="shopping-card-image"
-                    src={restaurant.image}
-                    alt={restaurant.name}
+                    src={restaurant.restaurant.restaurant_image}
+                    alt={restaurant.restaurant.name}
                 />
                 </Grid>
                 <Grid item lg={8} md={8} sm={8} xs={12}>
                 <Typography className="shopping-card-name">
-                    {restaurant.name}
+                    {restaurant.restaurant.name}
                 </Typography>
                 <Grid container spacing={1}>
-                    {restaurant.orders.map((order, index) => (
+                    {restaurant.items.map((order, index) => (
                     <>
                         <Grid item lg={12} md={8} sm={8} xs={12}>
                             <Typography>
-                            ({order.count}) {order.itemName} - Price: ${order.price}
+                            ({order.quantity}) {order.item.name} - Price: ${order.total_price_after_discount}
                             </Typography>
                         </Grid>
                     </>
