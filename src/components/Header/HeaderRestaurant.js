1bc6684c72f92bba7cc46b375b89e39f4ca12e8b
haniyehasadi <haniyeasadi627@gmail.com>
2023-12-22 21:16:53 +0330

    fix: solve the problem of header

--- src/components/Header/HeaderRestaurant.js
+++ src/components/Header/HeaderRestaurant.js
@@ -16,7 +16,8 @@ const HeaderRestaurant = memo(() => {
     const token = localStorage.getItem('token');
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
-    const val = JSON.parse(localStorage.getItem('email'));    
+    const val = JSON.parse(localStorage.getItem('email'));
+    const [blurBackground, setBlurBackground] = useState(false);    
 
     const handleClickProfile = () => {
         history.push("/edit-manager");
@@ -63,9 +64,11 @@ const HeaderRestaurant = memo(() => {
     };
 
     return ( 
-        <>
+        <div
+            className={`container ${blurBackground ? 'blur-background' : ''}`}
+        >
             <AppBar 
-                sx={{position:"sticky", width:'fixed', padding: '0'}} 
+                sx={{position:"sticky", width:'fixed', padding: '0 !important'}} 
                 className="header"
             >
                 <Toolbar className='header-toolbar'>
@@ -122,6 +125,7 @@ const HeaderRestaurant = memo(() => {
                                         overflow: 'visible',
                                         filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
                                         mt: 1,
+                                        ml: 0.5,
                                         '& .MuiAvatar-root': {
                                         width: 32,
                                         height: 32,
@@ -168,7 +172,7 @@ const HeaderRestaurant = memo(() => {
                     )}
                 </Toolbar>
             </AppBar>
-        </>
+        </div>
     );
 });
 
